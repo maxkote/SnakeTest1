16fa0f417ec16c601e40fbca23fe4244aae47bca
Maksymilian Kotecki <m.kotecki05@gmail.com>
2024-05-20 21:36:03 +0200

    ostatnie szlify

--- game.py
+++ game.py
@@ -5,15 +5,21 @@ import config
 
 class Game:
     def __init__(self):
+        self.init = pg.init()
         self.screen = pg.display.set_mode(config.SCREEN_SIZE)
         self.clock = pg.time.Clock()
-        self.init = pg.init()
         self.caption = pg.display.set_caption('Snake')
         self.font = pg.font.SysFont("arial", 45)
         self.running = True
+        self.exit = False
 
         self.snake = Snake(self.screen)
         self.food = Food(self.screen)
+
+    def new_game(self):
+        self.snake = Snake(self.screen)
+        self.food = Food(self.screen)
+        self.running = True
 
     def update(self):
         self.snake.update()
@@ -34,7 +40,7 @@ class Game:
         self.check_food_collision()
 
     def draw_score(self):
-        score_text = self.font.render(f"{self.snake.length - 1}", True, (255, 255, 255))
+        score_text = self.font.render(f"{self.snake.length - 1}", True, config.WHITE)
         self.screen.blit(score_text, [20.5, 14.5])
 
     def draw_grid(self):
@@ -60,9 +66,11 @@ class Game:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
+                self.exit = True
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_q:
                     self.running = False
+                    self.exit = True
                 if (event.key == pg.K_LEFT or event.key == pg.K_a) and self.snake.x_change == 0:
                     self.snake.x_change = -config.TILE_SIZE
                     self.snake.y_change = 0
@@ -76,11 +84,37 @@ class Game:
                     self.snake.y_change = config.TILE_SIZE
                     self.snake.x_change = 0
 
+    def check_loser_screen_input_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.exit = True
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_q:
+                    self.exit = True
+                elif event.key == pg.K_n:
+                    self.new_game()
+
+    def loser_screen(self):
+        self.screen.fill(config.PURPLE)
+        score_text = self.font.render(f'Score: {self.snake.length - 1}', True, config.WHITE)
+        new_game_text = self.font.render('New game (N)', True, config.WHITE)
+
+        self.screen.blit(score_text, (config.DIS_WIDTH // 2 - score_text.get_width() // 2, config.DIS_HEIGHT // 3))
+        self.screen.blit(new_game_text, (config.DIS_WIDTH // 2 - new_game_text.get_width() // 2, config.DIS_HEIGHT // 3 + 75))
+
+        pg.display.flip()
+
     def run(self):
-        while self.running:
-            self.check_events()
-            self.update()
-            self.draw()
+        while not self.exit:
+            while self.running:
+                self.check_events()
+                self.update()
+                self.draw()
+
+            if not self.exit:
+                self.loser_screen()
+                while not self.running and not self.exit:
+                    self.check_loser_screen_input_events()
 
         pg.quit()
         quit()
