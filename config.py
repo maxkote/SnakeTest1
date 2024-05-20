852160c80f3332f8170b136531883ac619fb886b
Maksymilian Kotecki <m.kotecki05@gmail.com>
2024-05-20 16:44:25 +0200

    poprawki

---
+++ config.py
@@ -0,0 +1,12 @@
+# screen
+DIS_WIDTH = 600
+DIS_HEIGHT = 400
+SCREEN_SIZE = (600, 400)
+# game_values
+TILE_SIZE = 20
+SNAKE_SPEED = 10
+# colors
+GREEN = (9, 232, 68)
+PURPLE = (131, 41, 227)
+RED = (214, 2, 2)
+DARK_GREEN =(78, 168, 13)
